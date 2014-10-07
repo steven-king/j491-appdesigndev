68286c630d493444b6d476a1d62635e710ec4523
steven-king <cujophoto@gmail.com>
2014-10-06 21:55:51 -0400

    adding database connection

---
+++ database-connect/sample-files/app.js
@@ -0,0 +1,22 @@
+
+//place functions for data connect here...
+
+
+
+
+
+
+function onDeviceReady() {
+    //needed only for mobile deployment
+    console.log("onDeviceReady()");
+        
+}
+
+$( document ).ready(function() {
+    //for browser use only
+    console.log( "document ready!" );
+    //function to populate menu  
+
+});
+
+
